27d9bdaf25ec9b895fc64c9088f81b225bd06f59
Nathan Herzing <nherzing@gmail.com>
2013-10-30 12:41:49 -0400

    Update description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-smartystreets "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library wrapping SmartyStreets' LiveAddress API."
+  :url "https://github.com/turbovote/clj-smartystreets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
