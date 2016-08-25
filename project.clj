a2d986dc5a5b6c2671875a857f55cc7fa6c66216
Wes Morgan <wesmorgan@icloud.com>
2016-08-25 10:19:55 -0600

    Update deps to latest versions

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-min-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.1.0"]
-                 [cheshire "5.5.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
-  :plugins [[lein-midje "3.1.0"]]
+                 [clj-http "3.1.0"]
+                 [cheshire "5.6.3"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]}}
+  :plugins [[lein-midje "3.2"]]
   :aliases {"test" ["with-profile" "+test" "midje"]})
