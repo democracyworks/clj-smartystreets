10eab3d767cb2fbc32ce66839b7d6617f744e66a
Wes Morgan <wesmorgan@icloud.com>
2016-08-25 10:23:03 -0600

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-smartystreets "0.1.6"
+(defproject clj-smartystreets "0.1.7-SNAPSHOT"
   :description "A Clojure library wrapping SmartyStreets' LiveAddress API."
   :url "https://github.com/democracyworks/clj-smartystreets"
   :license {:name "Eclipse Public License"
