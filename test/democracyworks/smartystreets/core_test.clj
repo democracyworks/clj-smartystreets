f2f9e4851a73248048d2bc998e10a13b8213b5af
Gordon Stratton <gordon@democracy.works>
2017-12-11 23:07:03 +0000

    Add 3-arity Client record constructor

--- test/democracyworks/smartystreets/core_test.clj
+++ test/democracyworks/smartystreets/core_test.clj
@@ -1,34 +1,42 @@
 (ns democracyworks.smartystreets.core-test
   (:require
    [clj-http.fake :as fake]
-   [clojure.test :refer [deftest is]]
-   [democracyworks.smartystreets.core :as ss]))
+   [clojure.test :refer [deftest is testing]]
+   [democracyworks.smartystreets.core :as smartystreets]))
 
 (deftest client
-  (let [c (ss/client "id" "token")]
-    (is (= ["id" "token"]
-           ((juxt :auth-id :auth-token) c)))))
+  (testing "2-arity version"
+    (let [c (smartystreets/client "id" "token")]
+      (is (= ["id" "token"]
+             ((juxt :auth-id :auth-token) c)))))
+  (testing "3-arity version"
+    (let [c (smartystreets/client "id" "token" {:foo "bar"})]
+      (is (= ["id" "token" {:foo "bar"}]
+             ((juxt :auth-id :auth-token :http-params) c)))))
+  (testing "bogus arguments not allowed"
+    (is (thrown? AssertionError
+                 (smartystreets/client nil nil nil)))))
 
 (deftest http-get
-  (let [c (ss/client "id" "token")]
+  (let [c (smartystreets/client "id" "token")]
     (fake/with-fake-routes {"http://localhost?auth-id=id&auth-token=token&foo=bar"
                             (fn [req]
                               {:status 200
                                :body "{}"})}
-      (is (= {} (-> (ss/http-get
+      (is (= {} (-> (smartystreets/http-get
                      c
                      "http://localhost"
                      {:foo "bar"})
                     :body))))))
 
 (deftest http-post
-  (let [c (ss/client "id" "token")]
+  (let [c (smartystreets/client "id" "token")]
     (fake/with-fake-routes {"http://localhost?auth-id=id&auth-token=token"
                             {:post
                              (fn [req]
                                {:status 200
                                 :body "[{}]"})}}
-      (is (= [{}] (-> (ss/http-post
+      (is (= [{}] (-> (smartystreets/http-post
                        c
                        "http://localhost"
                        {:foo "bar"})
