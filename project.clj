340251535ebe285a5c5660e9ba720362b9c2a40d
Gordon Stratton <gordon@democracy.works>
2018-01-05 17:42:57 +0000

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/clj-smartystreets "0.2.0"
+(defproject democracyworks/clj-smartystreets "0.2.1-SNAPSHOT"
   :description "A Clojure library wrapping SmartyStreets' Cloud APIs."
   :url "https://github.com/democracyworks/clj-smartystreets"
   :license {:name "Eclipse Public License"
