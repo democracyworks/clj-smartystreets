f74c1e8db0a5b67556691ecc53e0699b2503233a
Gordon Stratton <gordon@democracy.works>
2020-08-27 17:58:23 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,12 @@
   :url "https://github.com/democracyworks/clj-smartystreets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-http "3.7.0"]
-                 [cheshire "5.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-http "3.10.2"]
+                 [cheshire "5.10.0"]]
   :profiles {:dev {:dependencies [[clj-http-fake "1.0.3"]
-                                  [midje "1.9.1"]]
+                                  [midje "1.9.9"]]
                    :repl-options {:init-ns dev.user}
                    :source-paths ["dev-src"]}}
-  :plugins [[lein-midje "3.2.1"]]
-  :aliases {"test" ["do" ["with-profile" "+test" "midje"]
-                         ["test"]]})
+  :plugins [[lein-midje "3.2.2"]]
+  :aliases {"test" "midje"})
