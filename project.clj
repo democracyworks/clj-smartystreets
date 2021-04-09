e847d2c3535dfabf2b0a4c77e66c670e37acfd40
Gordon Stratton <gordon@democracy.works>
2021-04-09 14:25:48 -0700

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/democracyworks/clj-smartystreets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clj-http "3.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [clj-http "3.12.1"]
                  [cheshire "5.10.0"]]
   :profiles {:dev {:dependencies [[clj-http-fake "1.0.3"]
-                                  [midje "1.9.9"]]
+                                  [midje "1.9.10"]]
                    :repl-options {:init-ns dev.user}
                    :source-paths ["dev-src"]}}
   :plugins [[lein-midje "3.2.2"]]
